0b104a67ead7fec2ab420141842a86960fb9564a
gilberto <gcashere@gmail.com>
2026-02-21 19:37:10 -0600

    feat: primeros dos endpoints de solicitudes (post de user y get master) y correccion de relaciones Prisma

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use(express.json()); // Vital para leer los { body } que mande React
 const authRoutes = require('./routes/auth.routes');
 const catalogosRoutes = require('./routes/catalogos.routes');
 const activosRoutes = require('./routes/activos.routes');
-// const solicitudesRoutes = require('./routes/solicitudes.routes');
+const solicitudesRoutes = require('./routes/solicitudes.routes');
 // const mantenimientosRoutes = require('./routes/mantenimientos.routes');
 // const usuariosRoutes = require('./routes/usuarios.routes');
 // const dashboardRoutes = require('./routes/dashboard.routes');
@@ -24,7 +24,7 @@ const activosRoutes = require('./routes/activos.routes');
 app.use('/api/auth', authRoutes);                   // Módulo 1
 app.use('/api/catalogos', catalogosRoutes);         // Módulo 2 (Catálogos)
 app.use('/api/activos', activosRoutes);             // Módulo 2 (Inventario)
-// app.use('/api/solicitudes', solicitudesRoutes);     // Módulo 3
+app.use('/api/solicitudes', solicitudesRoutes);     // Módulo 3
 // app.use('/api/mantenimientos', mantenimientosRoutes); // Módulo 4
 // app.use('/api/usuarios', usuariosRoutes);           // Módulo 5
 // app.use('/api/dashboard', dashboardRoutes);         // Módulo 7
